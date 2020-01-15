a282664a71bc4578df0c318054f76e95a389857f
Oscar Kraemer <oscar.kraemer@csc.fi>
2020-01-15 14:23:34 +0200

    Make count_routers_on_host.sh more like master

--- neutron/count_routers_on_host.sh
+++ neutron/count_routers_on_host.sh
@@ -5,10 +5,16 @@
 # count the number of routers per host
 # optional argument of ACTIVE_ONLY -- which will count routers on active L3 agents only
 
+echo "Router count per host. Gathering data. Be patient."
 
-AGENTS=$(openstack network agent list --agent-type l3 -c ID -f value)
+if [ "$1" == "ACTIVE_ONLY" ]; then
+    AGENTS=$(neutron agent-list --column id --column agent_type --column host --format csv --quote minimal --column admin_state_up --column alive | grep ':-)' | grep True | grep L3 | cut -f1 -d',')
+else
+    AGENTS=$(neutron agent-list --column id --column agent_type --column host --format csv --quote minimal | grep L3 | cut -f1 -d',')
+fi
+
 for agent in ${AGENTS}; do
-  ROUTER_HOST=$(openstack network agent show ${agent} -c host -f value)
-  COUNT=$(neutron router-list-on-l3-agent -f value  ${agent} |wc -l)
-  echo  "${ROUTER_HOST} (${agent}): ${COUNT}"
+    COUNT=$(neutron router-list-on-l3-agent -f value  ${agent} |wc -l)
+    ROUTER_HOST=$(openstack network agent show ${agent} -c host -f value)
+    echo "${ROUTER_HOST} (${agent}): ${COUNT}"
 done
